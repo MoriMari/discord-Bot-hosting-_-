0c52b818fff3a41ba5c2bc2728867f0af39477ad
MoriMari <56265663+MoriMari@users.noreply.github.com>
2019-11-06 15:08:09 +0900

    Add files via upload

---
+++ strong money's pig(discord bot).py
@@ -0,0 +1,26 @@
+import discord
+from discord import Game
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("테스트 중")
+    await client.change_presence(status=discord.Status.online)
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("안녕"):
+        await message.channel.send("나도 안녕!")
+    if message.content.startswith("ㅎㅇㅎㅇ"):
+        await message.channel.send("하이하이!")
+    if message.content.startswith("능지처참누구?"):
+        await message.channel.send("강꼰대!!")
+    if message.content.startswith("우리돼지?"):
+        await message.channel.send("한돈!")
+
+client.run("NjM5ODQ5NzgzMDY1OTY4NjUw.XbxSag.crWKzH0XmR8kCiu6ihomFfSsShg")
